5552651bc668cb0ee8a8e52acf5dd1f537a0e6e4
gradwahl <argnara@gmail.com>
2026-09-19 00:28:17 +0100

    Add minigame bot Castle Wars join task

---
+++ src/main/java/com/rs2/bot/MinigameBotJoinTask.java
@@ -0,0 +1,19 @@
+package com.rs2.bot;
+
+import com.rs2.model.task.TickTask;
+
+public final class MinigameBotJoinTask
+extends TickTask {
+    private final BotPlayer botPlayer;
+
+    public MinigameBotJoinTask(int delay, BotPlayer botPlayer) {
+        super(delay);
+        this.botPlayer = botPlayer;
+    }
+
+    @Override
+    public final void execute() {
+        MinigameBotManager.joinCastleWars(this.botPlayer);
+        this.stop();
+    }
+}
