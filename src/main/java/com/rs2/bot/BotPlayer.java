eace599b3b663c87f55ebffaad266168ff5ad13c
gradwahl <argnara@gmail.com>
2026-09-19 00:48:05 +0100

    Spawn minigame bots at Castle Wars

--- src/main/java/com/rs2/bot/BotPlayer.java
+++ src/main/java/com/rs2/bot/BotPlayer.java
@@ -120,7 +120,11 @@ extends Player {
 
     @Override
     public final void initializeNewPlayer() {
-        this.moveTo(new Position(ServerSettings.respawnX, ServerSettings.respawnY, ServerSettings.respawnPlane));
+        if (this.botMode == 7) {
+            MinigameBotManager.placeAtCastleWarsLobby(this);
+        } else {
+            this.moveTo(new Position(ServerSettings.respawnX, ServerSettings.respawnY, ServerSettings.respawnPlane));
+        }
         this.randomizeAppearance();
         this.completeTutorial();
         this.completeAllQuestStates();
