9df82e70966da34edae3aa1cd804dedfb4ac2e4a
gradwahl <argnara@gmail.com>
2026-09-19 00:29:15 +0100

    Register minigames bot type

--- src/main/java/com/rs2/Server.java
+++ src/main/java/com/rs2/Server.java
@@ -125,6 +125,7 @@ implements Runnable {
     private static int progressiveBotLoginCount;
     private static int tradeBotLoginCount;
     private static int clanWarsBotLoginCount;
+    private static int minigameBotLoginCount;
     private static int otherBotLoginCount;
     public static int onlinePlayerCount;
     public static int adminPlayerCount;
@@ -151,6 +152,7 @@ implements Runnable {
         progressiveBotLoginCount = 0;
         tradeBotLoginCount = 0;
         clanWarsBotLoginCount = 0;
+        minigameBotLoginCount = 0;
         otherBotLoginCount = 0;
         onlinePlayerCount = 0;
         adminPlayerCount = 0;
@@ -374,6 +376,7 @@ implements Runnable {
             progressiveBotLoginCount = 0;
             tradeBotLoginCount = 0;
             clanWarsBotLoginCount = 0;
+            minigameBotLoginCount = 0;
             otherBotLoginCount = 0;
             if (ServerSettings.progressiveBotsEnabled) {
                 ServerSettings.walkingBotsEnabled = true;
@@ -393,6 +396,9 @@ implements Runnable {
             if (ServerSettings.clanWarsTeamSize <= 0 && ServerSettings.clanWarsBotsEnabled) {
                 ServerSettings.clanWarsBotsEnabled = false;
             }
+            if (ServerSettings.minigameBotCount <= 0 && ServerSettings.minigameBotsEnabled) {
+                ServerSettings.minigameBotsEnabled = false;
+            }
             configuredBotCount = 0;
             if (ServerSettings.wildyBotsEnabled) {
                 configuredBotCount += WildernessBotSettings.wildyBotCount;
@@ -408,6 +414,9 @@ implements Runnable {
             }
             if (ServerSettings.clanWarsBotsEnabled) {
                 configuredBotCount += ServerSettings.clanWarsTeamSize << 1;
+            }
+            if (ServerSettings.minigameBotsEnabled) {
+                configuredBotCount += ServerSettings.minigameBotCount;
             }
             if (ServerSettings.otherBotsEnabled) {
                 configuredBotCount += ServerSettings.otherBotCount;
@@ -528,6 +537,10 @@ implements Runnable {
             int value = ClanWarsBotManager.clanWarsTeamOneBots.size() == ServerSettings.clanWarsTeamSize ? 6 : 5;
             ++clanWarsBotLoginCount;
             return value;
+        }
+        if (ServerSettings.minigameBotsEnabled && minigameBotLoginCount < ServerSettings.minigameBotCount) {
+            ++minigameBotLoginCount;
+            return 7;
         }
         if (ServerSettings.otherBotsEnabled && otherBotLoginCount < ServerSettings.otherBotCount) {
             ++otherBotLoginCount;
