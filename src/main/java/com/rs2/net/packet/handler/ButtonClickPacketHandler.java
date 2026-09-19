e9967787b74d5f99d17e9e95ed37a11796ba1a67
gradwahl <argnara@gmail.com>
2026-09-19 17:07:20 +0100

    Handle Castle Wars replacement offer choices

--- src/main/java/com/rs2/net/packet/handler/ButtonClickPacketHandler.java
+++ src/main/java/com/rs2/net/packet/handler/ButtonClickPacketHandler.java
@@ -7,6 +7,7 @@ import com.rs2.model.EntityUpdateState;
 import com.rs2.model.GameplayHelper;
 import com.rs2.model.gameplay.barrows.BarrowsManager;
 import com.rs2.model.gameplay.castlewars.CastleWarsEngineeringManager;
+import com.rs2.model.gameplay.castlewars.CastleWarsManager;
 import com.rs2.model.gameplay.partyroom.PartyRoomManager;
 import com.rs2.model.ground.GroundItem;
 import com.rs2.model.ground.GroundItemManager;
@@ -53,6 +54,7 @@ implements PacketHandler {
         if (ServerSettings.debugModeEnabled) {
             System.out.println("button id: " + buttonId);
         }
+        if (CastleWarsManager.handleReplacementOfferButton(player, buttonId)) return;
         if (CastleWarsEngineeringManager.handleCatapultButton(player, buttonId)) return;
         if (player.getDuelSession().handleButtonClick(buttonId) != false) return;
         if (player.getQuestManager().handleButtonClick(buttonId) != false) return;
