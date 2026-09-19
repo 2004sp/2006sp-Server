ec0fb030d4303eec25a7ac85baf0f1355de175bb
gradwahl <argnara@gmail.com>
2026-09-19 20:53:06 +0100

    Handle cache-driven Lanthus fourth action

--- src/main/java/com/rs2/model/interaction/FourthNpcActionTask.java
+++ src/main/java/com/rs2/model/interaction/FourthNpcActionTask.java
@@ -1,9 +1,11 @@
 package com.rs2.model.interaction;
 
 import com.rs2.model.EntityTargetMovement;
+import com.rs2.model.gameplay.castlewars.CastleWarsManager;
 import com.rs2.model.npc.Npc;
 import com.rs2.model.player.BankManager;
 import com.rs2.model.player.Player;
+import com.rs2.model.shop.ShopManager;
 import com.rs2.model.task.TickTask;
 import com.rs2.util.GameUtil;
 
@@ -27,6 +29,17 @@ extends TickTask {
             return;
         }
         if (!this.player.isWithinReach(this.npc, 1) || this.player.isOverlapping(this.npc)) {
+            return;
+        }
+        // Raw NPC cache action slot 4 is packet 18 in the matching 2006 client.
+        if (this.npc.getNpcId() == CastleWarsManager.LANTHUS_NPC_ID
+                && this.npc.getDefinition().actionStartsWith(4, "trade")) {
+            this.npc.getUpdateState().setFaceEntity(this.player.getEncodedIndex());
+            this.player.setInteractionTarget(this.npc);
+            this.player.getUpdateState().setFaceEntity(this.npc.getEncodedIndex());
+            ShopManager.openCastleWarsRewardShop(this.player);
+            EntityTargetMovement.clearMovementTarget(this.player);
+            this.stop();
             return;
         }
         if (!GameUtil.hasClearPath(this.player.getPosition(), this.npc.getPosition(), true)) {
